2082419336ddae85a8c7c031febf051951f16735
Victor Melnyk <victor.melnyk.vm@gmail.com>
2013-12-29 22:37:15 +0000

    add simple page

---
+++ conf/a_blocks_features/.php
@@ -0,0 +1,9 @@
+<?php
+$lRootDir = '../../../';
+
+require_once($lRootDir.'blocks/comp/common/tools.php');
+require_once($lRootDir.'blocks/comp/blocks/.php');
+
+cPage::settingsGet()->rootDir = $lRootDir;
+cPage::moduleAdd('blocks_demo/conf_app/.php');
+?>
