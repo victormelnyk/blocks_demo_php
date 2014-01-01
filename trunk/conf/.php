02a8c06d6cc16b0c89676a833b244b514cde9bd5
vm <vm@26475ecb-b5ee-4350-9a03-9c4412b00345>
2014-01-01 13:28:52 +0000

    added sys demo

--- trunk/conf/.php
+++ trunk/conf/.php
@@ -2,7 +2,7 @@
 $lRootDir = '../../';
 
 require_once($lRootDir.'blocks/comp/common/tools.php');
-require_once($lRootDir.'blocks/comp/blocks/.php');
+require_once($lRootDir.'blocks/comp/blocks/blocks.php');
 
 cPage::settingsGet()->rootDir = $lRootDir;
 cPage::moduleAdd('blocks_demo/conf_app/.php');
