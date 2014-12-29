3e1961fe5f7fdbfa90ab7bdf3b8a8bbf267c9d34
vm <vm@26475ecb-b5ee-4350-9a03-9c4412b00345>
2014-12-29 22:07:52 +0000

    update demo

---
+++ trunk/conf/_.php
@@ -0,0 +1,5 @@
+<?
+require_once($settings['rootDir'] . 'blocks/comp/common/tools.php');
+require_once($settings['rootDir'] . 'blocks/comp/blocks/blocks.php');
+Page::createSettings($settings);
+?>
---
+++ trunk/web/tests/cache/index.php
@@ -0,0 +1,9 @@
+<?
+require_once('../../../../blocks/comp/common/tools.php');
+require_once('../../../../blocks/comp/blocks/blocks.php');
+require_once('../../../../blocks/ext/php_json_format_master/jsonformat.class.php');
+
+$cache = new Cache('', true);
+
+p('done');
+?>
